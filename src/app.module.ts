15e600c170bac658e9d3d4341c82fcb81c55d011
Thales Souza Chagas <thales.souz@outlook.com>
2024-05-18 10:10:10 -0300

    chore: app structure by domain

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MovieModule } from './movie/movie.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, MovieModule, CustomerModule],
 })
 export class AppModule {}
