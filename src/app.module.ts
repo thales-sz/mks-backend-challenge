24073780ccbecb42087a33643a8c4cc8a7d5ac84
Thales Souza Chagas <thales.souz@outlook.com>
2024-05-18 11:21:04 -0300

    feat: .env validation with Joi package

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { MovieModule } from './movie/movie.module';
 import { CustomerModule } from './customer/customer.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validationSchema: Joi.object({}),
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+      envFilePath: '.env',
     }),
     AuthModule,
     MovieModule,
