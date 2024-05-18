1b78f3410e08d437dd710dc4f8081de4b26e5fdc
Thales Souza Chagas <thales.souz@outlook.com>
2024-05-18 16:18:44 -0300

    feat(customer): add movie entity to customer context

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -4,9 +4,10 @@ import { CustomerController } from './customer.controller';
 import { AuthModule } from '../auth/auth.module';
 import { Customer } from '../database/models/customer.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from '../database/models/movie.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Customer, Movie]), AuthModule],
   controllers: [CustomerController],
   providers: [CustomerService],
 })
