dd9a30ec0686a6d70ceaa740b084ea7ff5af8526
Thales Souza Chagas <thales.souz@outlook.com>
2024-05-18 11:04:21 -0300

    feat: initial swagger documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MovieModule } from './movie/movie.module';
 import { CustomerModule } from './customer/customer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, MovieModule, CustomerModule],
+  imports: [ConfigModule.forRoot(), AuthModule, MovieModule, CustomerModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,49 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestApplication<AppModule> =
+    await NestFactory.create<NestExpressApplication>(AppModule);
+  const logger = new Logger(AppModule.name);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.setGlobalPrefix('api/v1');
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders:
+      'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('MKS Backend API')
+    .setDescription("API created for MKS' backend challenge")
+    .setVersion('1.0')
+    .addTag('Movies')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document);
+
+  app
+    .listen(configService.get('PORT'))
+    .then(async () =>
+      logger.log(
+        `Server is running on PORT: ${await configService.get('PORT')}`,
+      ),
+    );
 }
 bootstrap();
